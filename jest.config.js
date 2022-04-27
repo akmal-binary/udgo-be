780dcc948ca1c8495940b0aa9d1038a633c3aca1
Akmal Djumakhodjaev <akmal@binary.com>
2022-04-27 12:22:25 +0800

    Setup jest test

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
+  modulePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests', 'Boilerplate'],
   testEnvironment: 'node',
   testEnvironmentOptions: {
     NODE_ENV: 'test',
   },
   restoreMocks: true,
-  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
+  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests', 'Boilerplate'],
   coverageReporters: ['text', 'lcov', 'clover', 'html'],
 };
